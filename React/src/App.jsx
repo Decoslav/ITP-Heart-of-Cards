4179ca2ebf768795aac6f5c21e06372f7268cfc2
paulartn <if24b296@technikum-wien.at>
2026-03-18 12:25:40 +0100

    Mage Sprite added
    Mage Sprite added

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -18,9 +18,9 @@ function App() {
           <Route path="/deckbuilder" element={<h1>Baue dein 20-Karten-Deck</h1>} />
           <Route path="/deck" element={<div style={{ display: 'flex', gap: '20px', padding: '40px' }}>
       {/* Hier erstellen wir drei verschiedene Karten mit einer Komponente! */}
-      <Card name="Feuerdrache" hp={25} atk={8} emoji="🔥" />
+      <Card name="Feuerdrache" hp={25} atk={8} imageUrl="🔥" />
       <Card name="Eishexe" hp={20} atk={5} imageUrl="/images/hexe.png" />
-      <Card name="Ritter" hp={30} atk={4} emoji="⚔️" />
+      <Card name="Mage" hp={15} atk={10} imageUrl="/images/Mage.png" />
     </div>} />
           <Route path="/duell" element={<h1>Bereit zum Duell?</h1>} />
         </Routes>
