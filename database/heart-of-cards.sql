45699a79ebd04787309cdde0747f0f6dd78b667a
Elias <eliashorvatits@gmail.com>
2026-05-15 21:09:54 +0200

    modified database

--- database/heart-of-cards.sql
+++ database/heart-of-cards.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 07. Mai 2026 um 15:46
+-- Erstellungszeit: 15. Mai 2026 um 21:02
 -- Server-Version: 10.4.32-MariaDB
 -- PHP-Version: 8.2.12
 
@@ -24,6 +24,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `card`
+--
+
+CREATE TABLE `card` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `hp` int(11) NOT NULL,
+  `atk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `deck`
+--
+
+CREATE TABLE `deck` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `deck_cards`
+--
+
+CREATE TABLE `deck_cards` (
+  `deck_id` int(11) NOT NULL,
+  `card_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `users`
 --
 
@@ -37,8 +73,38 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Daten für Tabelle `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `username`, `password`, `role`, `created_at`) VALUES
+(1, 'versuch1@gmail.com', 'versuch1', '$2y$10$ZPK1NmeF1Ml/GruvWAKTpeSGg50fa5UZNhrJoMeHx.KXbi1/znlme', 'user', '2026-05-07 16:13:44'),
+(2, 'versuch2@gmail.com', 'versuch2', '$2y$10$p9QCEMCPZKj4Jx0PlDJ/guYYjNMKtX70npc9Bl2h/dVFYTmZ9XTcG', 'user', '2026-05-12 14:35:06'),
+(3, 'versuch3@gmail.com', 'versuch3', '$2y$10$vqMDcTQsaGAQsthi1mX4quzZQUZH1WM8fUgn/daNiuRfowzB5XVfm', 'user', '2026-05-12 14:50:53'),
+(4, 'versuch4@gmail.com', 'versuch4', '$2y$10$6da3dF.MmRaLmRDUrR7seu1X7P6oFIuV2YlqVb3Q17HHUWvvsyMj2', 'user', '2026-05-12 14:53:04');
+
+--
 -- Indizes der exportierten Tabellen
 --
+
+--
+-- Indizes für die Tabelle `card`
+--
+ALTER TABLE `card`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `deck`
+--
+ALTER TABLE `deck`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_deck_user` (`user_id`);
+
+--
+-- Indizes für die Tabelle `deck_cards`
+--
+ALTER TABLE `deck_cards`
+  ADD KEY `fk_deckcards_deck` (`deck_id`),
+  ADD KEY `fk_deckcards_card` (`card_id`);
 
 --
 -- Indizes für die Tabelle `users`
@@ -53,10 +119,39 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT für Tabelle `card`
+--
+ALTER TABLE `card`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `deck`
+--
+ALTER TABLE `deck`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `deck`
+--
+ALTER TABLE `deck`
+  ADD CONSTRAINT `fk_deck_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints der Tabelle `deck_cards`
+--
+ALTER TABLE `deck_cards`
+  ADD CONSTRAINT `fk_deckcards_card` FOREIGN KEY (`card_id`) REFERENCES `card` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_deckcards_deck` FOREIGN KEY (`deck_id`) REFERENCES `deck` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
